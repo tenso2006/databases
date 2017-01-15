4686e6155703da19b815f6f3d9eaa94e7eb78609
Tenzin Sonam <tenso2006@gmail.com>
2017-01-15 00:16:05 -0800

    created db connection

--- server/db/index.js
+++ server/db/index.js
@@ -5,50 +5,25 @@ var handler = require('../db');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-var connection = function() {
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'chat'
-  });
-  return connection;
-};
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
 
-module.exports = {
-  methods: {
-    input: function (input) {
-      var queryInput = input;
-      return queryInput;
-    },
 
-    query: function (input, table, column) {
-      var newQuery = connection();
-      newQuery.connect();
-      var array;
-      if (table === 'users') {
-        array = [input.toString];
-        userQuery.query('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")', function (err, results) {
-          if (err) {
-            console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")');
-          } else {
-            console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")');
-          }
-        });
-      } else {
-        input.forEach(function(input) {
-          array.push(input.toString());
-        });
-        messageQuery.query('INSERT INTO ' + table + ' (' + column + ') Values ("' + array[0] + '","' + array[1] + '""' + array[2] + '")', function (err, results) {
-          if (err) {
-            console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array[0] + '","' + array[1] + '""' + array[2] + '")');
-          } else {
-            console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array[0] + '","' + array[1] + '""' + array[2] + '")');
-          }
-        });
-      }
-      newQuery.end();
-    }
+
+connection.connect(function (err){
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
   }
-};
+  console.log('Connection to db established');
+});
+
+
+
+module.exports.connection = connection;
+
 
