6a4aec5a3d03b671f96aa03ac823ce47e62a625d
Sean O'Donnell <sean.odonnell.cfa@gmail.com>
2017-01-14 16:43:10 -0800

    connected to db

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,6 @@
 var mysql = require('mysql');
 var handler = require('../db');
 
-var queryInput;
-
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
@@ -20,23 +18,23 @@ var connection = function() {
 module.exports = {
   methods: {
     input: function (input) {
-      queryInput = input;
+      var queryInput = input;
       return queryInput;
     },
 
     query: function (input, table, column) {
       var newQuery = connection();
       newQuery.connect();
-      var post  = { user_name: input };
+      var post  = { username: input };
       var array = [];
-      array.push(post.user_name.toString());
+      array.push(post.username.toString());
 
       newQuery.query('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")', function (err, results) {
         if (err) {
           console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")');
         } else {
           console.log('INSERT INTO ' + table + ' (' + column + ') Values ("' + array + '")');
-          }
+        }
       });
       newQuery.end();
     }
