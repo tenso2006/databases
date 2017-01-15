9f9b67ad5d6ea43b5ebe56d23bcaef06066d8db6
Tenzin Sonam <tenso2006@gmail.com>
2017-01-15 00:02:31 -0800

    ran db

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,21 @@
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
+
+DROP TABLE IF EXISTS `messages`;
+
 
 CREATE TABLE messages (
   id int NOT NULL AUTO_INCREMENT,
   message varchar(255) NOT NULL,
   room_name varchar(20) NOT NULL,
   user_name varchar(30) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
+
+
+DROP TABLE IF EXISTS `users`;
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE users (
